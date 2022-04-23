4eeaf21820dae5a1b95afdff609625cd4391a444
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-04-23 13:27:48 -0400

    05-sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(one,two) {
+    let firstNum = one;
+    let secondNum = two
+    let sum = 0;
+    if(one < 0 || two < 0){
+        return "ERROR"
+    }
+    if(typeof one !== typeof two){
+        return "ERROR"
+    }
+    if(one>two){
+        firstNum = two;
+        secondNum = one
+        for(firstNum;firstNum<=secondNum; firstNum++){
+            sum += firstNum
+        }
+        return sum
+    }else{
+        for(one;one<=two; one++){
+        sum += one
+    }
+        return sum
+    }
+    
 };
 
 // Do not edit below this line
