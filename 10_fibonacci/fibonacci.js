cf1d62869cdc0cddb1ab222407f164f6005ce565
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-05-03 14:11:10 -0400

    10_fibonacci complete. Had to google more than I wouldve liked for this one

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let n1 = 0, n2=1, nextTerm;
 
-};
+    if(a < 0){
+        return "OOPS"
+    }else{
+        for(let i = 1; i<=a; i++){
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+            console.log(n1)
+        }
+        return n1
+    }
+    
+    
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
